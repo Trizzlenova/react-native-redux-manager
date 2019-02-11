e24c6569a75ff4aadda4cb8354e7c1b89bdb7b22
Trizzlenova <tdswayzee@gmail.com>
2019-02-11 14:23:36 -0800

    adding features to employeecreate

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,5 +1,6 @@
 import {
   EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
 } from '../actions/types';
 
 const INITIAL_STATE = {
@@ -11,9 +12,11 @@ const INITIAL_STATE = {
 export default (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case EMPLOYEE_UPDATE:
-    //[] is key interpolation, adding to the object will be
+    // [] is key interpolation, adding to the object will be
     // determined at run time
       return { ...state, [action.payload.prop]: action.payload.value };
+    case EMPLOYEE_CREATE:
+      return INITIAL_STATE;
     default:
       return state;
   }
